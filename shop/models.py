c500fb0328fb46b8de69fdba63117a87417e8f58
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-16 14:33:32 +0000

    Create 'Category' model in models.py

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -26,28 +28,28 @@ class Product(models.Model):
         return self.name
 
 
-#class A_image(models.Model):
-#    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#    image = models.ImageField(upload_to='other_images/')
+class A_image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='other_images/')
 
-#    def __str__(self):
-#        return f"{self.product.name}' other image"
-
-
-#class A_color(models.Model):
-#    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#    color = models.CharField(max_length=50)
-
-#    def __str__(self):
-#        return f"{self.product.name}' other color"
+    def __str__(self):
+        return f"{self.product.name}' other image"
 
 
-#class A_size(models.Model):
-    #product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    #size = models.CharField(max_length=50)
+class A_color(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    color = models.CharField(max_length=50)
 
-    #def __str__(self):
-    #    return f"{self.product.name}' other size"
+    def __str__(self):
+        return f"{self.product.name}' other color"
+
+
+class A_size(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.product.name}' other size"
 
 
 Grocery = 'Grocery'
@@ -82,15 +84,15 @@ category_choices = [(Grocery, 'Grocery'), (Phones_Tablets, 'Phones & Tablets'), 
                     ]
 
 
-#class Category(models.Model):
-#    category = models.CharField(max_length=100, choices=category_choices)
-#    slug = models.SlugField(max_length=355, unique=True)
+class Category(models.Model):
+    category = models.CharField(max_length=100, choices=category_choices)
+    slug = models.SlugField(max_length=355, unique=True)
 
-#    def get_absolute_url(self):
-#        return reverse("shop:categories_list", args=[self.slug])
+    def get_absolute_url(self):
+        return reverse("shop:categories_list", args=[self.slug])
 
-#    class Meta:
-#        verbose_name_plural = 'Categories'
+    class Meta:
+        verbose_name_plural = 'Categories'
 
-#   def __str__(self):
-#        return self.category
+    def __str__(self):
+        return self.category
