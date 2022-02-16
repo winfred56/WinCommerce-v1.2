8228b02f77c965815915900d08201afa6a803cd4
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-16 15:09:33 +0000

    Create 'views' in shop/views.py

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path()
+    path('', views.home, name='home'),
+    path('category/<slug:slug>/', views.categories_list, name='category_list'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+
+def categories(request):
+    return {'cat': Category.objects.all()}
+
+
+def home(request):
+    products = Product.objects.all()
+    search = request.GET.get('search')
+    if search != " " and search is not None:
+        products = Product.objects.filter(name__icontains=search)
+    context = {'products': products}
+    return render(request, 'shop/home.html', context)
+
+
+def categories_list(request, slug):
+    category = Category.objects.get(slug=slug)
+    products = Product.objects.filter(category=category)
+    context = {
+        'category': category,
+        'products': products
+    }
+    return render(request, 'shop/category.html', context)
+
+
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    return render(request, 'shop/detail.html', context={'product': product})
