fe6fdc13e52ba097decc116887871f94a3c42a97
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-22 22:25:52 +0000

    Include 'accounts.urls' in core.urls

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),
     path('basket/', include('basket.urls', namespace='basket')),
+    path('accounts/', include('accounts.urls', namespace='accounts'))
 ]
 
 if settings.DEBUG:
