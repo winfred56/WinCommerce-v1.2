07c826b5191554c848ab915e358f9a9914437819
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-22 22:26:18 +0000

    registration view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import UserBase
+from .forms import RegistrationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.user_name = form.cleaned_data['user_name']
+            user.email = form.cleaned_data['email']
+            user.set_password(form.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            # activation Email
+    else:
+        form = RegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+
+
