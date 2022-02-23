533d11d0e16adb613cb3d7a5bd239b3b897fe376
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-23 12:36:04 +0000

    remove an unnecessary bracket from path

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('activate/<slug:uidb64>/<slug:token>)/', views.account_activate, name='activate'),
+    path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
 ]
