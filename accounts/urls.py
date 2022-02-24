5840c089393c92e20c677d703e1e48ba7fc2c598
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-24 23:01:05 +0000

    Create dashboard path

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,10 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
+
     path('login/',
          auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
