64d19fbb575074267b1025c21e2b92d1b4844558
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-24 22:36:28 +0000

    Remove form_class

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
     path('login/',
-         auth_views.LoginView.as_view(template_name='accounts/login.html', form_class=LoginForm), name='login'),
+         auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
 ]
