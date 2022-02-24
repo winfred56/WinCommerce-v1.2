31144a697295d4118672010860b41c2441ad9d29
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-24 22:21:59 +0000

    create 'Login' & 'Logout' path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .forms import LoginForm
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='accounts/login.html', form_class=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
 ]
