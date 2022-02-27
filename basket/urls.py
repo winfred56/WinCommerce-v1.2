1dba7eda2e85fa2580eba2f33af019728122b231
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-27 18:50:51 +0000

    Refactor

--- basket/urls.py
+++ basket/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "basket"
 urlpatterns = [
     path('add_to_cart/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
-    path('remove_from_cart/<slug:slug>/', views.remove_from_cart, name='remove_from_cart'),
+    path('remove_cart_item/<slug:slug>/', views.remove_cart_item, name='remove_cart_item'),
     path('cart/', views.cart, name='cart'),
     path('remove_single_item_from_cart/<slug:slug>/', views.remove_single_item_from_cart,
          name='remove_single_item_from_cart'),
