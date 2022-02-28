c966ebe49a0019b92f91aa8f3a125a578570aa58
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-02-28 06:27:23 +0000

    Delete 'Remove_single_item_from_cart' path

--- basket/urls.py
+++ basket/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('add_to_cart/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
     path('remove_cart_item/<slug:slug>/', views.remove_cart_item, name='remove_cart_item'),
     path('cart/', views.cart, name='cart'),
-    path('remove_single_item_from_cart/<slug:slug>/', views.remove_single_item_from_cart,
-         name='remove_single_item_from_cart'),
+
 ]
