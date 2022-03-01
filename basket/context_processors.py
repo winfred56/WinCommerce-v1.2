2c16f77cff2a6a4d5b9ed838ab5aecba227a1152
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-03-01 10:45:36 +0000

    Create 'context_processors.py'

---
+++ basket/context_processors.py
@@ -0,0 +1,10 @@
+from .models import Cart
+
+def counter(request):
+    cart_ = Cart.objects.filter(user=request.user, ordered=False)
+    if cart_.exists():
+        cart = cart_[0]
+        if cart.products.exists():
+            up = cart.products.count()
+            print(up)
+            return {'count': up}
