6efea3d51cd3ecc293e888e1d6973e005f42c9f6
Winfred Adu-Acheampong <winfred242002@gmail.com>
2022-03-03 08:49:15 +0000

    Checkout path

--- basket/urls.py
+++ basket/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add_to_cart/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
     path('remove_cart_item/<slug:slug>/', views.remove_cart_item, name='remove_cart_item'),
     path('cart/', views.cart, name='cart'),
-    # path('checkout/', views.checkout, name='checkout'),
+    path('checkout/', views.checkout, name='checkout'),
+
 ]
